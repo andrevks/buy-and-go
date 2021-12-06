28ae3d77544bb62e33781f11ec7e56acdad73318
andrevks <italiabk@gmail.com>
2021-12-06 12:25:22 -0400

    refactor: addProductScreen

--- lib/screens/add_product_screen.dart
+++ lib/screens/add_product_screen.dart
@@ -39,89 +39,12 @@ class _AddProductScreenState extends State<AddProductScreen> {
                 Container(
                   height: 30,
                 ),
-                // TextFormField(
-                //   controller: _controllerTotalPrice,
-                //   decoration: InputDecoration(
-                //     labelText: 'Preço Total',
-                //     labelStyle: const TextStyle(color: Colors.blue),
-                //     hintText: 'Informe o Preço Total',
-                //   ),
-                // ),
-                // TextFormField(
-                //   controller: _controllerAge,
-                //   decoration: InputDecoration(
-                //     labelText: 'Idade',
-                //     labelStyle: TextStyle(color: Colors.blue),
-                //     hintText: 'Informe a idade',
-                //   ),
-                // ),
                 SizedBox(
                   height: 40,
                 ),
                 ElevatedButton(
-                  onPressed: () async {
-                    //  final List<  =  Database.productList();
-
-                    // Future<List<Object?>> allProduct =  Database.getData();
-                    List<Map<String, dynamic>> allProducts =
-                        await Database.getData();
-
-                    // print("Generate Purchase: $allProducts");
-
-                    print("-----------------------\n");
-                    print("LENGTH: ${allProducts.length}");
-
-                    int len = allProducts.length;
-
-                    var rng = Random();
-                    int nItems = 1;
-                    for (int i = 0; i < 20; i++) {
-                      nItems = rng.nextInt(len);
-                      if (nItems > 0) {
-                        print("Number of Items: $nItems");
-                        break;
-                      }
-                    }
-
-                    print("-----------------------\n");
-
-                    double totalPrice = 0.0;
-                    List<Map<String, dynamic>> purchasedItems = [];
-                    for (int i = 0; i < nItems; i++) {
-                      Map<String, dynamic> element =
-                          allProducts[rng.nextInt(len)];
-
-                      if (purchasedItems.contains(element)) {
-                        print(
-                            '${element['name']} is present in the list $purchasedItems');
-                        element['qty'] += 1;
-                      } else {
-                        print(
-                            '${element['name']} is not present in the list$purchasedItems');
-                        element['qty'] = 1;
-                        purchasedItems.add(element);
-                      }
-                    }
-
-                    print("productsBought:$purchasedItems");
-
-                    totalPrice = purchasedItems.fold(0, (prev, element) {
-                      double actualPrice = (element['qty'] * element['price']);
-                      print("actualPrice: $actualPrice");
-                      return prev + actualPrice;
-                    });
-
-                    print("totalPrice $totalPrice");
-                    String stringDate =
-                        DateFormat('yyyy-MM-dd').format(DateTime.now());
-                    Database.addToShoppingList(
-                        date: stringDate,
-                        totalPrice: totalPrice,
-                        products: purchasedItems
-                        // age: _controllerAge.text.isEmpty
-                        //     ? null
-                        //     : int.parse(_controllerAge.text),
-                        );
+                  onPressed: () {
+                    generateProduct();
                     Navigator.of(context).pop();
                   },
                   child: Text("Gerar Compra"),
@@ -131,72 +54,55 @@ class _AddProductScreenState extends State<AddProductScreen> {
           ],
         ),
       )),
-
-      // body: Form(
-      //     child: Padding(
-      //   padding: const EdgeInsets.all(20.0),
-      //   child: ListView(
-      //     children: [
-      //       Column(
-      //         children: [
-      //           SizedBox(
-      //             height: 30,
-      //           ),
-      //           Text(
-      //             'Cadastro de Estudantes',
-      //             style: TextStyle(
-      //               fontSize: 30,
-      //             ),
-      //             textAlign: TextAlign.center,
-      //           ),
-      //           Container(
-      //             height: 30,
-      //           ),
-      //           TextFormField(
-      //             controller: _controllerName,
-      //             decoration: InputDecoration(
-      //               labelText: 'Nome do Estudante',
-      //               labelStyle: const TextStyle(color: Colors.blue),
-      //               hintText: 'Inform o nome do estudante',
-      //             ),
-      //           ),
-      //           TextFormField(
-      //             controller: _controllerSocialMedia,
-      //             decoration: InputDecoration(
-      //               labelText: 'Redes Sociais',
-      //               labelStyle: const TextStyle(color: Colors.blue),
-      //               hintText: 'Informe a rede social',
-      //             ),
-      //           ),
-      //           TextFormField(
-      //             controller: _controllerAge,
-      //             decoration: InputDecoration(
-      //               labelText: 'Idade',
-      //               labelStyle: TextStyle(color: Colors.blue),
-      //               hintText: 'Informe a idade',
-      //             ),
-      //           ),
-      //           SizedBox(
-      //             height: 40,
-      //           ),
-      //           ElevatedButton(
-      //             onPressed: () {
-      //               Database.addStudent(
-      //                 name: _controllerName.text,
-      //                 socialMedia: _controllerSocialMedia.text,
-      //                 age: _controllerAge.text.isEmpty
-      //                     ? null
-      //                     : int.parse(_controllerAge.text),
-      //               );
-      //               Navigator.of(context).pop();
-      //             },
-      //             child: Text("Adicionar Estudante"),
-      //           )
-      //         ],
-      //       ),
-      //     ],
-      //   ),
-      // )),
     );
+  }
+
+  void generateProduct() async {
+    List<Map<String, dynamic>> allProducts = await Database.getData();
+
+    print("-----------------------");
+    print("LENGTH: ${allProducts.length}");
+
+    int len = allProducts.length;
+
+    var rng = Random();
+    int nItems = 1;
+    for (int i = 0; i < 20; i++) {
+      nItems = rng.nextInt(len);
+      if (nItems > 0) {
+        print("Number of Items: $nItems");
+        break;
+      }
+    }
+
+    print("-----------------------\n");
+
+    double totalPrice = 0.0;
+    List<Map<String, dynamic>> purchasedItems = [];
+    for (int i = 0; i < nItems; i++) {
+      Map<String, dynamic> element = allProducts[rng.nextInt(len)];
+
+      if (purchasedItems.contains(element)) {
+        print('${element['name']} is present in the list $purchasedItems');
+        element['qty'] += 1;
+      } else {
+        print('${element['name']} is not present in the list$purchasedItems');
+        element['qty'] = 1;
+        purchasedItems.add(element);
+      }
+    }
+
+    print("productsBought:$purchasedItems");
+
+    totalPrice = purchasedItems.fold(0, (prev, element) {
+      double actualPrice = (element['qty'] * element['price']);
+      print("actualPrice: $actualPrice");
+      return prev + actualPrice;
+    });
+
+    print("totalPrice $totalPrice");
+    String stringDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
+    Database.addToShoppingList(
+        date: stringDate, totalPrice: totalPrice, products: purchasedItems);
   }
 }
